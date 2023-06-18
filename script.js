4d83a05811e158ee4b09f83522bae1c941389f7f
Tiffanie Lim <tiffanielim3147@gmail.com>
2023-06-17 20:36:32 -0700

    update questions to reflect a Git user's lifestyle and the cardinal sin of Git

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const questions = [
-      {
-        question: 'Which Git command is used to create a new branch?',
-        choices: ['git checkout', 'git branch', 'git commit'],
-        correctAnswer: 1
-      },
-      {
-        question: 'What does "git push" do?',
-        choices: ['Fetches changes from a remote repository', 'Synchronizes the local repository with a remote repository', 'Adds changes to the staging area'],
-        correctAnswer: 1
-      },
-      {
-        question: 'How can you discard local changes in Git?',
-        choices: ['git reset', 'git commit', 'git revert'],
-        correctAnswer: 0
-      },
-        {
-          question: 'What Git command is used to create and switch to a new branch in one step?',
-          choices: ['git branch', 'git checkout -b', 'git merge'],
-          correctAnswer: 1
-        },
-        {
-          question: 'What is the purpose of the "git clone" command?',
-          choices: ['To create a new Git repository', 'To copy an existing Git repository to your local machine', 'To merge two branches in Git'],
-          correctAnswer: 1
-        },
-        {
-          question: 'What does "git merge" do?',
-          choices: ['Fetches changes from a remote repository', 'Synchronizes the local repository with a remote repository', 'Combines changes from one branch into another'],
-          correctAnswer: 2
-        },
-        {
-          question: 'How can you undo the last Git commit?',
-          choices: ['git reset', 'git revert', 'git checkout'],
-          correctAnswer: 0
-        },
-        {
-          question: 'What is the purpose of the "git stash" command?',
-          choices: ['To permanently delete changes', 'To save changes temporarily and revert to the last commit', 'To publish changes to a remote repository'],
-          correctAnswer: 1
-        },
+  const questions = [
+    {
+      question: 'What is the first step to initialize a new Git repository?',
+      choices: ['git clone', 'git init', 'git commit'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you add files to the staging area?',
+      choices: ['git commit', 'git add', 'git push'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the next step after adding files to the staging area?',
+      choices: ['git clone', 'git commit', 'git push'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What does "git commit" do?',
+      choices: ['Saves the changes to the local repository', 'Updates the remote repository', 'Creates a new branch'],
+      correctAnswer: 0
+    },
+    {
+      question: 'How do you create a new branch?',
+      choices: ['git checkout -b', 'git branch', 'git merge'],
+      correctAnswer: 0
+    },
+    {
+      question: 'What is the purpose of branching in Git?',
+      choices: ['To save changes temporarily', 'To isolate and work on a new feature or task', 'To discard local changes'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you switch to a different branch?',
+      choices: ['git merge', 'git checkout', 'git commit'],
+      correctAnswer: 1
+    },
+    {
+      question: 'After completing work on a branch, how do you merge it back to the main branch?',
+      choices: ['git push', 'git pull', 'git merge'],
+      correctAnswer: 2
+    },
+    {
+      question: 'What is the final step to share your changes with others?',
+      choices: ['git commit', 'git push', 'git pull'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you update your local repository with the latest changes from a remote repository?',
+      choices: ['git push', 'git pull', 'git clone'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the purpose of "git fetch"?',
+      choices: ['To discard local changes', 'To update the local repository with remote changes', 'To create a new branch'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the purpose of "git remote"?',
+      choices: ['To create a new repository', 'To list and manage remote repositories', 'To merge branches in Git'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you discard local changes and revert to the last committed state?',
+      choices: ['git reset', 'git revert', 'git checkout'],
+      correctAnswer: 0
+    },
+    {
+      question: 'What does "git pull" do?',
+      choices: ['Fetches changes from a remote repository and merges them into the current branch', 'Synchronizes the local repository with a remote repository', 'Adds changes to the staging area'],
+      correctAnswer: 0
+    },
         {
           question: 'What is the cardinal sin of Git?',
           choices: ['Not committing regularly', 'Forgetting to create branches', 'Pushing directly onto main'],
