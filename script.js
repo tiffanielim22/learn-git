43843dbe4ddb00f33514d2b0dd3c86031b4ddd4d
tiffanielim22 <136932521+tiffanielim22@users.noreply.github.com>
2023-06-17 19:26:06 -0700

    [learngit] Add Git quiz feature (#1)
    * add git quiz
    
    * add JS quiz
    
    * add more questions
    
    * changed questions
    
    * separate questions into another JS file
    
    * Revert "separate questions into another JS file"
    
    This reverts commit b5cd56034b6085cfa2a4f956abdc1abf6eb4a5c1.
    
    ---------
    
    Co-authored-by: Tiffanie Lim <tiffanielim3147@gmail.com>

---
+++ script.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const questions = [
+    {
+      question: 'What is the first step to initialize a new Git repository?',
+      choices: ['git clone', 'git init', 'git commit'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you add files to the staging area?',
+      choices: ['git commit', 'git add', 'git push'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the next step after adding files to the staging area?',
+      choices: ['git clone', 'git commit', 'git push'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What does "git commit" do?',
+      choices: ['Saves the changes to the local repository', 'Updates the remote repository', 'Creates a new branch'],
+      correctAnswer: 0
+    },
+    {
+      question: 'How do you create a new branch?',
+      choices: ['git checkout -b', 'git branch', 'git merge'],
+      correctAnswer: 0
+    },
+    {
+      question: 'What is the purpose of branching in Git?',
+      choices: ['To save changes temporarily', 'To isolate and work on a new feature or task', 'To discard local changes'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you switch to a different branch?',
+      choices: ['git merge', 'git checkout', 'git commit'],
+      correctAnswer: 1
+    },
+    {
+      question: 'After completing work on a branch, how do you merge it back to the main branch?',
+      choices: ['git push', 'git pull', 'git merge'],
+      correctAnswer: 2
+    },
+    {
+      question: 'What is the final step to share your changes with others?',
+      choices: ['git commit', 'git push', 'git pull'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you update your local repository with the latest changes from a remote repository?',
+      choices: ['git push', 'git pull', 'git clone'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the purpose of "git fetch"?',
+      choices: ['To discard local changes', 'To update the local repository with remote changes', 'To create a new branch'],
+      correctAnswer: 1
+    },
+    {
+      question: 'What is the purpose of "git remote"?',
+      choices: ['To create a new repository', 'To list and manage remote repositories', 'To merge branches in Git'],
+      correctAnswer: 1
+    },
+    {
+      question: 'How do you discard local changes and revert to the last committed state?',
+      choices: ['git reset', 'git revert', 'git checkout'],
+      correctAnswer: 0
+    },
+    {
+      question: 'What does "git pull" do?',
+      choices: ['Fetches changes from a remote repository and merges them into the current branch', 'Synchronizes the local repository with a remote repository', 'Adds changes to the staging area'],
+      correctAnswer: 0
+    }
+  ];
+  
+    let currentQuestionIndex = 0;
+    let score = 0;
+  
+    const questionElement = document.getElementById('question');
+    const choicesContainer = document.getElementById('choices');
+    const scoreElement = document.getElementById('score');
+  
+    function displayQuestion() {
+      const currentQuestion = questions[currentQuestionIndex];
+      questionElement.textContent = currentQuestion.question;
+  
+      choicesContainer.innerHTML = '';
+      currentQuestion.choices.forEach((choice, index) => {
+        const choiceButton = document.createElement('button');
+        choiceButton.textContent = choice;
+        choiceButton.addEventListener('click', () => checkAnswer(index));
+        choicesContainer.appendChild(choiceButton);
+      });
+    }
+  
+    function checkAnswer(selectedIndex) {
+      const currentQuestion = questions[currentQuestionIndex];
+      if (selectedIndex === currentQuestion.correctAnswer) {
+        score++;
+      }
+      currentQuestionIndex++;
+  
+      if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+      } else {
+        showResult();
+      }
+    }
+  
+    function showResult() {
+      questionElement.textContent = 'Quiz completed!';
+      choicesContainer.innerHTML = '';
+      scoreElement.textContent = `Your score: ${score}/${questions.length}`;
+    }
+  
+    displayQuestion();
+  });  
